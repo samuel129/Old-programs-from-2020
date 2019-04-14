7d33a573dd6cd43665e8fbbf3444cd44174d8952
samuel129 <41707531+samuel129@users.noreply.github.com>
2019-04-14 00:10:05 -0700

    Add files via upload

---
+++ Aziz C++ Assign4.cpp
@@ -0,0 +1,28 @@
+//Solutions to the quadratic equation Ax^2 + Bx + C = 0 may be calculated using the quadratic formulas:
+
+// x = (-B + (B^2-4AC)^1/2)/2A or x = (-B -(B^2 -4AC)^1/2)/2A
+
+//These formulas may be used, of course, only if the leading coefficient, A, is not zero. The number and type of 
+//solutions is determined by the value of the expression under the radical sign, B^2 - 4AC, known as the discriminant:
+
+//Value of discriminant: | positive |   zero   |   negative   |
+//Number of solutions:   |     2    |     1    |       2      |
+//Kind of solutions:     |   real   |   real   |   imaginary  |
+
+// Your job is to write a program which will read the coefficients of a quadratic equation and, if the leading coefficient
+// is non-zero, calculate and report the solutions. Since the programming languages does not provide the imaginary type, youll
+// have to take appropriate steps to give imaginary results in te form shown. Just remember that every imaginary number is really
+// determined by a pair of real numbers.
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout<<"Hello World";
+    discriminant = cin << 
+
+    return 0;
+}
+
